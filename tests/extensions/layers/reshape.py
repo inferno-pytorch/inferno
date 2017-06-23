3693a3991179d0824fcd3602e5e6303d4e60c2f1
Nasim Rahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-06-23 21:23:02 +0200

    - tests for the new reshape function (passing)

--- tests/extensions/layers/reshape.py
+++ tests/extensions/layers/reshape.py
@@ -2,26 +2,57 @@ import unittest
 
 
 class TestReshape(unittest.TestCase):
-    def test_as_matrix(self):
+    def _get_input_variable(self, *shape):
         import torch
-        from inferno.extensions.layers.reshape import AsMatrix
-        from torch.autograd import Variable
+        return torch.autograd.Variable(torch.rand(*shape))
 
-        input = Variable(torch.rand(10, 20, 1, 1))
+    def test_as_matrix(self):
+        from inferno.extensions.layers.reshape import AsMatrix
+
+        input = self._get_input_variable(10, 20, 1, 1)
         as_matrix = AsMatrix()
         output = as_matrix(input)
         self.assertEqual(list(output.size()), [10, 20])
 
     def test_flatten(self):
-        import torch
         from inferno.extensions.layers.reshape import Flatten
-        from torch.autograd import Variable
 
-        input = Variable(torch.rand(10, 20, 2, 2))
+        input = self._get_input_variable(10, 20, 2, 2)
         flatten = Flatten()
         output = flatten(input)
         self.assertEqual(list(output.size()), [10, 80])
 
+    def test_as_2d(self):
+        from inferno.extensions.layers.reshape import As2D
+
+        as_2d = As2D()
+
+        output_shape = as_2d(self._get_input_variable(10, 20, 3, 30, 30)).size()
+        self.assertEqual(list(output_shape), [10, 60, 30, 30])
+
+        output_shape = as_2d(self._get_input_variable(10, 20, 30, 30)).size()
+        self.assertEqual(list(output_shape), [10, 20, 30, 30])
+
+        output_shape = as_2d(self._get_input_variable(10, 20)).size()
+        self.assertEqual(list(output_shape), [10, 20, 1, 1])
+
+    def test_as_3d(self):
+        from inferno.extensions.layers.reshape import As3D
+
+        as_3d = As3D()
+
+        output_shape = as_3d(self._get_input_variable(10, 20, 3, 30, 30)).size()
+        self.assertEqual(list(output_shape), [10, 20, 3, 30, 30])
+
+        output_shape = as_3d(self._get_input_variable(10, 20, 30, 30)).size()
+        self.assertEqual(list(output_shape), [10, 20, 1, 30, 30])
+
+        output_shape = as_3d(self._get_input_variable(10, 20)).size()
+        self.assertEqual(list(output_shape), [10, 20, 1, 1, 1])
+
+        as_3d.channel_as_z = True
+        output_shape = as_3d(self._get_input_variable(10, 20, 30, 30)).size()
+        self.assertEqual(list(output_shape), [10, 1, 20, 30, 30])
 
 if __name__ == '__main__':
     unittest.main()
