fc9d3549ccfa339d2878ede06d98688ae5b7f185
Nasim Rahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-06-23 21:12:04 +0200

    - new reshape layers

--- inferno/extensions/layers/reshape.py
+++ inferno/extensions/layers/reshape.py
@@ -31,3 +31,50 @@ class AsMatrix(View):
 class Flatten(View):
     def __init__(self):
         super(Flatten, self).__init__(as_shape=['x', -1])
+
+
+class As3D(nn.Module):
+    def __init__(self, channel_as_z=False):
+        super(As3D, self).__init__()
+        self.channel_as_z = channel_as_z
+
+    def forward(self, input):
+        if input.dim() == 5:
+            # If input is a batch of 3D volumes - return as is
+            return input
+        elif input.dim() == 4:
+            # If input is a batch of 2D images, reshape
+            b, c, _0, _1 = list(input.size())
+            if self.channel_as_z:
+                # Move channel axis to z
+                return input.view(b, 1, c, _0, _1)
+            else:
+                # Keep channel axis where it is, but add a singleton dimension for z
+                return input.view(b, c, 1, _0, _1)
+        elif input.dim() == 2:
+            # We have a matrix which we wish to turn to a 3D batch
+            b, c = list(input.size())
+            return input.view(b, c, 1, 1, 1)
+        else:
+            raise NotImplementedError
+
+
+class As2D(nn.Module):
+    def __init__(self, z_as_channel=True):
+        super(As2D, self).__init__()
+        self.z_as_channel = z_as_channel
+
+    def forward(self, input):
+        if input.dim() == 5:
+            b, c, _0, _1, _2 = list(input.size())
+            if not self.z_as_channel:
+                assert _0 == 1
+            # Reshape
+            return input.view(b, c * _0, _1, _2)
+        elif input.dim() == 4:
+            # Nothing to do here - input is already 2D
+            return input
+        elif input.dim() == 2:
+            # We make singleton dimensions
+            b, c = list(input.size())
+            return input.view(b, c, 1, 1)
