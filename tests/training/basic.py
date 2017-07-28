3bbcc5e391db056c809e1ff8f5c4f7e8543709d2
Nasim Rahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-07-28 21:53:02 +0200

    - elementary test for the multi-io machinery (needs more testing)

--- tests/training/basic.py
+++ tests/training/basic.py
@@ -1,13 +1,15 @@
 from unittest import TestCase
 from unittest import main
 import time
+from os.path import join, dirname
 
 
 class TestTrainer(TestCase):
     # Parameters
-    ROOT_DIR = __file__
-    CUDA = True
-    HALF_PRECISION = True
+    ROOT_DIR = dirname(__file__)
+    CUDA = False
+    HALF_PRECISION = False
+    DOWNLOAD_CIFAR = False
 
     @staticmethod
     def _make_test_model():
@@ -15,11 +17,14 @@ class TestTrainer(TestCase):
         from inferno.extensions.layers.reshape import AsMatrix
 
         toy_net = nn.Sequential(nn.Conv2d(3, 128, 3, 1, 1),
+                                nn.ELU(),
                                 nn.MaxPool2d(2),
                                 nn.Conv2d(128, 128, 3, 1, 1),
+                                nn.ELU(),
                                 nn.MaxPool2d(2),
                                 nn.Conv2d(128, 256, 3, 1, 1),
-                                nn.AdaptiveMaxPool2d((1, 1)),
+                                nn.ELU(),
+                                nn.AdaptiveAvgPool2d((1, 1)),
                                 AsMatrix(),
                                 nn.Linear(256, 10),
                                 nn.Softmax())
@@ -27,56 +32,24 @@ class TestTrainer(TestCase):
 
     def test_cifar(self):
         from inferno.trainers.basic import Trainer
-        from inferno.trainers.callbacks.essentials import NaNDetector
-        import torch
-        import torchvision
-        from torchvision import transforms
-        import os
-
-        # Data preparation. Borrowed from
-        # https://github.com/kuangliu/pytorch-cifar/blob/master/main.py
-        transform_train = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-
-        transform_test = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-
-        trainset = torchvision.datasets.CIFAR10(root=os.path.join(self.ROOT_DIR, 'data'),
-                                                train=True, download=False,
-                                                transform=transform_train)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True,
-                                                  num_workers=2)
-
-        testset = torchvision.datasets.CIFAR10(root=os.path.join(self.ROOT_DIR, 'data'),
-                                               train=False, download=False,
-                                               transform=transform_test)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False,
-                                                 num_workers=2)
-
+        from inferno.io.box.cifar10 import get_cifar10_loaders
+        # Build cifar10 loaders
+        trainloader, testloader = get_cifar10_loaders(root_directory=join(self.ROOT_DIR, 'data'),
+                                                      download=self.DOWNLOAD_CIFAR)
         # Make model
         net = self._make_test_model()
-
         tic = time.time()
-
         # Make trainer
         trainer = Trainer(model=net)\
             .build_optimizer('Adam')\
             .build_criterion('CrossEntropyLoss')\
             .build_metric('CategoricalError')\
             .validate_every((1, 'epochs'))\
-            .save_every((1, 'epochs'), to_directory=os.path.join(self.ROOT_DIR, 'saves'))\
+            .save_every((1, 'epochs'), to_directory=join(self.ROOT_DIR, 'saves'))\
             .save_at_best_validation_score()\
-            .set_max_num_epochs(2)\
-
+            .set_max_num_epochs(2)
         # Bind trainer to datasets
         trainer.bind_loader('train', trainloader).bind_loader('validate', testloader)
-
         # Check device and fit
         if self.CUDA:
             if self.HALF_PRECISION:
@@ -85,10 +58,54 @@ class TestTrainer(TestCase):
                 trainer.cuda().fit()
         else:
             trainer.fit()
-
         toc = time.time()
-
         print("[*] Elapsed time: {} seconds.".format(toc - tic))
+
+    def test_multi_io(self):
+        from torch.utils.data.dataset import Dataset
+        from torch.utils.data.dataloader import DataLoader
+        from inferno.trainers.basic import Trainer
+        import torch
+
+        class DummyDataset(Dataset):
+            def __len__(self):
+                return 42
+
+            def __getitem__(self, item):
+                # 2 inputs and 3 targets (say)
+                return torch.rand(3, 32, 32), \
+                       torch.rand(3, 32, 32), \
+                       torch.rand(1).uniform_(), \
+                       torch.rand(1).uniform_(), \
+                       torch.rand(1).uniform_()
+
+        class DummyNetwork(torch.nn.Module):
+            def __init__(self):
+                super(DummyNetwork, self).__init__()
+                self.conv = torch.nn.Conv2d(3, 1, 3, padding=1)
+
+            def forward(self, *inputs):
+                assert len(inputs) == 2
+                out = self.conv(inputs[0])
+                return out.view(inputs[0].size(0), -1).mean(1), \
+                       out.view(inputs[0].size(0), -1).mean(1), \
+                       out.view(inputs[0].size(0), -1).mean(1)
+
+        class DummyCriterion(torch.nn.Module):
+            def forward(self, predictions, targets):
+                assert len(predictions) == len(targets) == 3
+                return predictions[0].mean()
+
+        loader = DataLoader(DummyDataset())
+        net = DummyNetwork()
+
+        trainer = Trainer(net)\
+            .build_criterion(DummyCriterion)\
+            .build_optimizer('Adam')\
+            .set_max_num_iterations(50)\
+            .bind_loader('train', loader, num_inputs=2, num_targets=3)
+
+        trainer.fit()
 
     def test_serialization(self):
         from inferno.trainers.basic import Trainer
@@ -146,6 +163,7 @@ class TestTrainer(TestCase):
 
 if __name__ == '__main__':
     tester = TestTrainer()
-    tester.ROOT_DIR = '/export/home/nrahaman/Python/Repositories/inferno/tests/training/root'
-    tester.test_multi_gpu()
-    # tester.test_serialization()
+    # tester.ROOT_DIR = '/export/home/nrahaman/Python/Repositories/inferno/tests/training/root'
+    # tester.test_multi_io()
+    tester.DOWNLOAD_CIFAR = True
+    tester.test_cifar()
