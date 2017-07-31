01706bc57fb5a0343db76da0440bb8b6de7a96d0
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-07-31 16:03:53 +0200

    - new file for exceptions and error handling

---
+++ inferno/utils/exceptions.py
@@ -0,0 +1,11 @@
+"""Exceptions and Error Handling"""
+
+
+def assert_(condition, message='', exception_type=AssertionError):
+    """Like assert, but with arbitrary exception types."""
+    if not condition:
+        raise exception_type(message)
+
+
+class ShapeError(ValueError):
+    pass
