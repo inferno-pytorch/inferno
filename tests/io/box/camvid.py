29e7e51b0e6f7983b838306a34776f6767415763
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-08-15 19:40:49 +0200

    - whitebox test for camvid - updated requirements

--- tests/io/box/camvid.py
+++ tests/io/box/camvid.py
@@ -1,10 +1,12 @@
 import os
+from os.path import join, dirname, exists, isdir
 import unittest
 import numpy as np
 
 
 class TestCamvid(unittest.TestCase):
     CAMVID_ROOT = None
+    PLOT_DIRECTORY = join(dirname(__file__), 'plots')
 
     def get_camvid_root(self):
         if self.CAMVID_ROOT is None:
@@ -26,7 +28,9 @@ class TestCamvid(unittest.TestCase):
     def test_camvid_dataset_with_transforms(self):
         from inferno.io.box.camvid import CamVid
         from inferno.io.transform.base import Compose
-        from inferno.io.transform.image import PILImage2NumPyArray
+        from inferno.io.transform.image import PILImage2NumPyArray, RandomSizedCrop, Scale
+        from inferno.utils.io_utils import print_tensor
+
         camvid = CamVid(self.get_camvid_root(),
                         image_transform=Compose(),
                         label_transform=Compose(),
@@ -36,6 +40,26 @@ class TestCamvid(unittest.TestCase):
         image, label = camvid[0]
         self.assertSequenceEqual(image.shape, (3, 360, 480))
         self.assertSequenceEqual(label.shape, (360, 480))
+        # Add crop trafo
+        camvid.joint_transform.add(RandomSizedCrop(ratio_between=(0.7, 1.0),
+                                                   preserve_aspect_ratio=True))
+        # We need 2 scale transforms, one with order 3 (image) and the other with order 0 (label)
+        camvid.joint_transform.add(Scale(output_image_shape=(360, 480),
+                                         interpolation_order=3, apply_to=[0]))
+        camvid.joint_transform.add(Scale(output_image_shape=(360, 480),
+                                         interpolation_order=0, apply_to=[1]))
+        image, label = camvid[0]
+        self.assertSequenceEqual(image.shape, (3, 360, 480))
+        self.assertSequenceEqual(label.shape, (360, 480))
+        self.assertLessEqual(len(np.unique(label)), 12)
+        # Print tensors to make sure they look legit
+        if not exists(self.PLOT_DIRECTORY):
+            os.mkdir(self.PLOT_DIRECTORY)
+        else:
+            assert isdir(self.PLOT_DIRECTORY)
+        print_tensor(image[None, ...], prefix='IMG--', directory=self.PLOT_DIRECTORY)
+        print_tensor(label[None, None, ...], prefix='LAB--', directory=self.PLOT_DIRECTORY)
+        print("[+] Inspect images at {}".format(self.PLOT_DIRECTORY))
 
 if __name__ == '__main__':
     tester = TestCamvid()
