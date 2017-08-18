9ff727237f2f4dc1b4d31f631be274abfad36ac2
Sven Peter <sven.peter@iwr.uni-heidelberg.de>
2017-08-18 15:28:58 +0200

    Add SaveFilenameCallback

--- inferno/trainers/callbacks/essentials.py
+++ inferno/trainers/callbacks/essentials.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 from ...utils import torch_utils as tu
 from .base import Callback
 
@@ -11,3 +12,21 @@ class NaNDetector(Callback):
             training_loss = training_loss.float()[0]
         if np.isnan(training_loss):
             raise RuntimeError("NaNs detected!")
+
+
+class SaveFilenameCallback(Callback):
+    def __init__(self, template='checkpoint.pytorch.epoch{epoch_count}.iteration{iteration_count}'):
+        self.template = template
+
+    def begin_of_save(self, **kwargs):
+        self._orig_checkpoint_filename = self.trainer._checkpoint_filename
+        self.trainer._checkpoint_filename = self.template.format(**kwargs)
+
+    def end_of_save(self, save_to_directory, **_):
+        orig_checkpoint_path = os.path.join(save_to_directory, self._orig_checkpoint_filename)
+
+        if os.path.lexists(orig_checkpoint_path):
+            os.remove(orig_checkpoint_path)
+        os.symlink(self.trainer._checkpoint_filename, orig_checkpoint_path)
+
+        self.trainer._checkpoint_filename = self._orig_checkpoint_filename
