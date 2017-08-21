294ae8801abbc7bf8ab5106f66d2c4ff721bb710
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-08-21 16:49:55 +0200

    Used cookiecutter template to improve the package

--- setup.py
+++ setup.py
@@ -1,28 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
 from setuptools import setup, find_packages
 
-# This will be set to true when the release is ready
-RELEASED = False
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
-if RELEASED:
-    VERSION = '0.0.1'
-    LONG_DESCRIPTION = "A utility library and training bench for Pytorch."
-    setup_info = dict(
-        name='inferno',
-        version=VERSION,
-        author='Nasim Rahaman',
-        author_email='nasim.rahaman@iwr.uni-heidelberg.de',
-        url='https://github.com/nasimrahaman/inferno',
-        description='Pytorch Training Bench',
-        long_description=LONG_DESCRIPTION,
-        license='Apache License 2.0',
-        packages=find_packages(exclude=('tests',)),
-        install_requires=[
-            'torch'
-        ]
-    )
-    setup(**setup_info)
-else:
-    message = "The packaging is not quite ready yet. If you still wish to use inferno on Linux\n" \
-              "or OSX, please navigate to the directory where this file is placed and run this\n" \
-              "command on a bash shell: `source add2path.sh`"
-    print()
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+    pytorch
+]
+
+setup_requirements = [
+    # TODO(nasimrahaman): put setup requirements (distutils extensions, etc.) here
+
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+    unittest
+]
+
+setup(
+    name='inferno',
+    version='0.1.0',
+    description="Inferno is a little library providing utilities and convenience functions/classes around PyTorch.",
+    long_description=readme + '\n\n' + history,
+    author="Nasim Rahaman",
+    author_email='nasim.rahaman@iwr.uni-heidelberg.de',
+    url='https://github.com/nasimrahaman/inferno',
+    packages=find_packages(include=['inferno']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords='inferno',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
---
+++ tests/test_inferno.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `inferno` package."""
+
+
+import unittest
+
+from inferno import inferno
+
+
+class TestInferno(unittest.TestCase):
+    """Tests for `inferno` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
