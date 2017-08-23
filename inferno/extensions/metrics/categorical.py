2e3ed87429eca982929b5a0d7354d528ff508e06
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-08-23 11:07:03 +0200

    - finished IOU with onehot tensors (not tested yet)

--- inferno/extensions/metrics/categorical.py
+++ inferno/extensions/metrics/categorical.py
@@ -1,7 +1,7 @@
 import torch
 from .base import Metric
-from ...utils.torch_utils import flatten_samples
-from ...utils.exceptions import assert_
+from ...utils.torch_utils import flatten_samples, is_label_tensor
+from ...utils.exceptions import assert_, DTypeError, ShapeError
 
 
 class CategoricalError(Metric):
@@ -47,22 +47,42 @@ class IOU(Metric):
         #   prediction.shape = (N, C, H, W)
         #   prediction.shape = (N, C, D, H, W)
         #   prediction.shape = (N, C)
-        # The corresponding target shapes are:
+        # The corresponding target shapes are either:
         #   target.shape = (N, H, W)
         #   target.shape = (N, D, H, W)
         #   target.shape = (N,)
+        # Or:
+        #   target.shape = (N, C, H, W)
+        #   target.shape = (N, C, D, H, W)
+        #   target.shape = (N, C)
         # First, reshape prediction to (C, -1)
         flattened_prediction = flatten_samples(prediction)
-        # Reshape target to (1, -1) for it to work with scatter
-        flattened_target = target.view(1, -1)
-        # Convert target to onehot with shape (C, -1)
-        num_classes, num_samples = flattened_prediction.size()
-        # Make sure the target is consistent
-        assert_(target.max() < num_classes)
-        onehot_targets = flattened_prediction\
-            .new(num_classes, num_samples)\
-            .zero_()\
-            .scatter_(0, flattened_target, 1)
+        # We need to figure out if the target is a int label tensor or a onehot tensor.
+        # The former always has one dimension less, so
+        if target.dim() == (prediction.dim() - 1):
+            # Labels, we need to go one hot
+            # Make sure it's a label
+            assert_(is_label_tensor(target),
+                    "Target must be a label tensor (of dtype long) if it has one "
+                    "dimension less than the prediction.",
+                    DTypeError)
+            # Reshape target to (1, -1) for it to work with scatter
+            flattened_target = target.view(1, -1)
+            # Convert target to onehot with shape (C, -1)
+            num_classes, num_samples = flattened_prediction.size()
+            # Make sure the target is consistent
+            assert_(target.max() < num_classes)
+            onehot_targets = flattened_prediction \
+                .new(num_classes, num_samples) \
+                .zero_() \
+                .scatter_(0, flattened_target, 1)
+        elif target.dim() == prediction.dim():
+            # Onehot, nothing to do except flatten
+            onehot_targets = flatten_samples(target)
+        else:
+            raise ShapeError("Target must have the same number of dimensions as the "
+                             "prediction, or one less. Got target.dim() = {} but "
+                             "prediction.dim() = {}.".format(target.dim(), prediction.dim()))
         # Now to compute the IOU = (a * b).sum()/(a**2 + b**2 - a * b).sum()
         # = (a * b).sum()/((a - b)**2).sum()
         # We sum over all samples and average over all classes
