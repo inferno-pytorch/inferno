b7f37d44ba1652832a016f064e5ebf48a19592e9
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-08-24 14:41:49 +0200

    Bump version: 1.1.3 → 1.1.4

--- inferno/__init__.py
+++ inferno/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Nasim Rahaman"""
 __email__ = 'nasim.rahaman@iwr.uni-heidelberg.de'
-__version__ = '__version__ = '1.1.3''
+__version__ = '__version__ = '1.1.4''
--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ dependency_links  = [
 
 setup(
     name='pytorch-inferno',
-    version='version='1.1.3'',
+    version='version='1.1.4'',
     description="Inferno is a little library providing utilities and convenience functions/classes around PyTorch.",
     long_description=readme + '\n\n' + history,
     author="Nasim Rahaman",
