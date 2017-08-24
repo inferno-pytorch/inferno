e1b54d8c8b885e326e32bd6b35790d79d5322e8e
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-08-24 14:19:00 +0200

    Bump version: 0.1.2 → 0.1.2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ dependency_links  = [
 
 setup(
     name='pytorch-inferno',
-    version='0.1.2',
+    version='version='0.1.2'',
     description="Inferno is a little library providing utilities and convenience functions/classes around PyTorch.",
     long_description=readme + '\n\n' + history,
     author="Nasim Rahaman",
