aa0c4bfdeb0ebeaf5a1d80e6415e7def17270737
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-08-25 19:43:02 +0200

    - fixed bug when computing classwise IOU

--- tests/extensions/metrics/categorical.py
+++ tests/extensions/metrics/categorical.py
@@ -23,5 +23,16 @@ class TestCategorical(unittest.TestCase):
         iou = IOU(ignore_class=1)(predicted_image[None, ...], target_image[None, ...])
         self.assertAlmostEqual(iou, expected_iou, places=4)
 
+    def test_multiclass_iou(self):
+        predicted_image = torch.zeros(*(2, 10, 10))
+        predicted_image[0, 0:4, 0:4] = 1
+        target_image = torch.zeros(*(2, 10, 10))
+        target_image[:, 0:3, 0:3] = 1
+        iou_class_0 = (3 * 3) / (4 * 4)
+        iou_class_1 = 0
+        expected_mean_iou = 0.5 * (iou_class_0 + iou_class_1)
+        iou = IOU()(predicted_image[None, ...], target_image[None, ...])
+        self.assertAlmostEqual(iou, expected_mean_iou, places=4)
+
 if __name__ == '__main__':
     unittest.main()
