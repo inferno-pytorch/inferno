31297e5ac765ff09bcade63201ab0f62192f4d9d
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-09-01 10:28:57 +0200

    - even more init imports for docs

--- inferno/__init__.py
+++ inferno/__init__.py
@@ -2,6 +2,11 @@
 
 """Top-level package for inferno."""
 
+from . import extensions
+from . import io
+from . import trainers
+from . import utils
+
 __author__ = """Nasim Rahaman"""
 __email__ = 'nasim.rahaman@iwr.uni-heidelberg.de'
 __version__ = '0.1.7'
