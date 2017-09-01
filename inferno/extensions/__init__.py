d98968295717ea9986104138c6d99a353e566875
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-09-01 12:15:14 +0200

    - more alls

--- inferno/extensions/__init__.py
+++ inferno/extensions/__init__.py
@@ -4,3 +4,5 @@ from . import initializers
 from . import layers
 from . import metrics
 from . import optimizers
+
+__all__ = ['containers', 'criteria', 'initializers', 'layers', 'metrics', 'optimizers']
