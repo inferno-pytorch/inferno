b441f03a26454454448d28238d9ceb60dcfb1376
abailoni <bailoni.alberto@gmail.com>
2017-12-21 12:50:35 +0100

    Added channelwise and weight options to Generalized Dice Score and minor changes

--- inferno/extensions/criteria/set_similarity_measures.py
+++ inferno/extensions/criteria/set_similarity_measures.py
@@ -26,8 +26,14 @@ class SorensenDiceLoss(nn.Module):
         self.channelwise = channelwise
         self.eps = eps
 
-    # TODO move weight here as optional argument ?!
     def forward(self, input, target):
+        """
+        input:      torch.FloatTensor or torch.cuda.FloatTensor
+        target:     torch.FloatTensor or torch.cuda.FloatTensor
+
+        Expected shape of the inputs: (batch_size, nb_channels, ...)
+        """
+        assert input.size() == target.size()
         if not self.channelwise:
             numerator = (input * target).sum()
             denominator = (input * input).sum() + (target * target).sum()
@@ -49,6 +55,7 @@ class SorensenDiceLoss(nn.Module):
                     channelwise_loss = channelwise_loss.squeeze(1)
                 # Wrap weights in a variable
                 weight = Variable(self.weight, requires_grad=False)
+                assert weight.size() == channelwise_loss.size()
                 # Apply weight
                 channelwise_loss = weight * channelwise_loss
             # Sum over the channels to compute the total loss
@@ -56,7 +63,6 @@ class SorensenDiceLoss(nn.Module):
         return loss
 
 
-# TODO we could also make the channel-wise ?!
 class GeneralizedDiceLoss(nn.Module):
     """
     Computes the scalar Generalized Dice Loss defined in https://arxiv.org/abs/1707.03237
@@ -64,24 +70,72 @@ class GeneralizedDiceLoss(nn.Module):
     This version works for multiple classes and expects predictions for every class (e.g. softmax output) and
     one-hot targets for every class.
     """
-    def __init__(self, eps=1e-6):
+    def __init__(self, weight=None, channelwise=False, eps=1e-6):
         super(GeneralizedDiceLoss, self).__init__()
+        self.register_buffer('weight', weight)
+        self.channelwise = channelwise
         self.eps = eps
 
-    def forward(self, prediction, target):
-        # Flatten input and target to have the shape (C, N),
-        # where N is the number of samples
-        prediction = flatten_samples(prediction)
-        target = flatten_samples(target)
+    def forward(self, input, target):
+        """
+        input: torch.FloatTensor or torch.cuda.FloatTensor
+        target:     torch.FloatTensor or torch.cuda.FloatTensor
 
-        # Find classes weights:
-        sum_targets = target.sum(-1)
-        class_weigths = 1. / (sum_targets * sum_targets).clamp(min=self.eps)
+        Expected shape of the inputs:
+            - if not channelwise: (batch_size, nb_classes, ...)
+            - if channelwise:     (batch_size, nb_channels, nb_classes, ...)
+        """
+        assert input.size() == target.size()
+        if not self.channelwise:
+            # Flatten input and target to have the shape (nb_classes, N),
+            # where N is the number of samples
+            input = flatten_samples(input)
+            target = flatten_samples(target)
 
-        # # Compute generalized Dice loss:
-        numer = ((prediction * target).sum(-1) * class_weigths).sum()
-        denom = ((prediction + target).sum(-1) * class_weigths).sum()
+            # Find classes weights:
+            sum_targets = target.sum(-1)
+            class_weigths = 1. / (sum_targets * sum_targets).clamp(min=self.eps)
 
-        loss = 1. - 2. * numer / denom.clamp(min=self.eps)
+            # Compute generalized Dice loss:
+            numer = ((input * target).sum(-1) * class_weigths).sum()
+            denom = ((input + target).sum(-1) * class_weigths).sum()
+
+            loss = 1. - 2. * numer / denom.clamp(min=self.eps)
+        else:
+            def flatten_and_preserve_channels(tensor):
+                tensor_dim = tensor.dim()
+                assert  tensor_dim >= 3
+                num_channels = tensor.size(1)
+                num_classes = tensor.size(2)
+                # Permute the channel axis to first
+                permute_axes = list(range(tensor_dim))
+                permute_axes[0], permute_axes[1], permute_axes[2] = permute_axes[1], permute_axes[2], permute_axes[0]
+                permuted = tensor.permute(*permute_axes).contiguous()
+                flattened = permuted.view(num_channels, num_classes, -1)
+                return flattened
+
+            # Flatten input and target to have the shape (nb_channels, nb_classes, N)
+            input = flatten_and_preserve_channels(input)
+            target = flatten_and_preserve_channels(target)
+
+            # Find classes weights:
+            sum_targets = target.sum(-1)
+            class_weigths = 1. / (sum_targets * sum_targets).clamp(min=self.eps)
+
+            # Compute generalized Dice loss:
+            numer = ((input * target).sum(-1) * class_weigths).sum(-1)
+            denom = ((input + target).sum(-1) * class_weigths).sum(-1)
+
+            channelwise_loss = 1. - 2. * numer / denom.clamp(min=self.eps)
+
+            if self.weight is not None:
+                if channelwise_loss.dim() == 2:
+                    channelwise_loss = channelwise_loss.squeeze(1)
+                channel_weights = Variable(self.weight, requires_grad=False)
+                assert channel_weights.size() == channelwise_loss.size(), "`weight` should have shape (nb_channels, ), `target` should have shape (batch_size, nb_channels, nb_classes, ...)"
+                # Apply channel weights:
+                channelwise_loss = channel_weights * channelwise_loss
+
+            loss = channelwise_loss.sum()
 
         return loss
