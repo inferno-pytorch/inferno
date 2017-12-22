950093301567debb02a3e7a9fea4f5916bb6091f
abailoni <bailoni.alberto@gmail.com>
2017-12-22 16:22:53 +0100

    Improved test for generalized dice score

--- tests/extensions/criteria/set_similarity_measures.py
+++ tests/extensions/criteria/set_similarity_measures.py
@@ -9,6 +9,12 @@ class SetSimilarityTest(unittest.TestCase):
         y = Variable(torch.zeros(3, 2, 100, 100).uniform_())
         return x, y
 
+    def get_dummy_variables_with_channels_and_classes(self):
+        # (batch_size, channels, classes, ...)
+        x = Variable(torch.zeros(3, 2, 5, 100, 100).uniform_())
+        y = Variable(torch.zeros(3, 2, 5, 100, 100).uniform_())
+        return x, y
+
 
 class TestSorensenDice(SetSimilarityTest):
     # noinspection PyCallingNonCallable
@@ -28,13 +34,18 @@ class TestSorensenDice(SetSimilarityTest):
 
 
 class TestGeneralizedSorensenDice(SetSimilarityTest):
-    # TODO better test
-    def test_pass(self):
+    def test_channelwise(self):
         from inferno.extensions.criteria.set_similarity_measures import GeneralizedDiceLoss
-        x, y = self.get_dummy_variables()
-        loss = GeneralizedDiceLoss()
-        val = loss(x, y)
-        self.assertLess(val, 0)
+        x, y = self.get_dummy_variables_with_channels_and_classes()
+        channelwise = GeneralizedDiceLoss(channelwise=True)
+        not_channelwise = GeneralizedDiceLoss(channelwise=False)
+        # Compute channelwise loss and expected one:
+        channelwise_loss = channelwise(x, y)
+        expected_channelwise_loss = \
+            not_channelwise(x[:, 0, ...], y[:, 0, ...]) + \
+            not_channelwise(x[:, 1, ...], y[:, 1, ...])
+        # Compare
+        self.assertAlmostEqual(expected_channelwise_loss.data[0], channelwise_loss.data[0])
 
 
 if __name__ == '__main__':
