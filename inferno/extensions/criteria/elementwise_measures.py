b8ff538e5e59cfd5d9500fd23c0cecfac8ca28b5
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-01-15 14:55:24 -0500

    Add weighted BCELoss (no test yet)

--- inferno/extensions/criteria/elementwise_measures.py
+++ inferno/extensions/criteria/elementwise_measures.py
@@ -36,3 +36,17 @@ class WeightedMSELoss(nn.Module):
             # `weights` should be positive if NEGATIVE_CLASS_WEIGHT is not messed with.
             sqrt_weights = Variable(weights.sqrt_(), requires_grad=False)
             return self.mse(input * sqrt_weights, target * sqrt_weights)
+
+
+class WeightedBCELoss(nn.Module):
+    def __init__(self, weight=None):
+        super(WeightedBCELoss, self).__init__()
+        if weight is not None:
+            assert torch.is_tensor(weight)
+        self.weight = weight
+        self.bce = nn.BCELoss()
+
+    def forward(self, input, target):
+        if self.weight is not None:
+            self.bce.weight = torch.FloatTensor([self.weight[1] / self.weight[0]])
+        return self.bce(input=input.view(-1), target=target.view(-1))
