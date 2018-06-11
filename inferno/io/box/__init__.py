fa4bcc62574073377886c9c439dd138a3c9637df
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2018-06-11 21:44:00 +0200

    - remove camvid

--- inferno/io/box/__init__.py
+++ inferno/io/box/__init__.py
@@ -1,6 +1,7 @@
 """Things that work out of the box. ;)"""
 
-from .camvid import CamVid, get_camvid_loaders
+# FIXME Camvid requires some deprecated torch function
+# from .camvid import CamVid, get_camvid_loaders
 from .cityscapes import Cityscapes, get_cityscapes_loaders
 from .cifar import get_cifar10_loaders, get_cifar100_loaders
 from .bsd500 import BSD500, get_bsd500_loaders, get_bsd500_dataset
