c70aee0cd7e70ca38b25edb7fd4260f88fa1e6be
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2018-06-18 18:19:45 +0200

    - put camvid in __init__ again (it was fixed in a previous commit) - remove bsd500 from this branch (has neurofire dependency)

--- inferno/io/box/__init__.py
+++ inferno/io/box/__init__.py
@@ -1,7 +1,5 @@
 """Things that work out of the box. ;)"""
 
-# FIXME Camvid requires some deprecated torch function
-# from .camvid import CamVid, get_camvid_loaders
+from .camvid import CamVid, get_camvid_loaders
 from .cityscapes import Cityscapes, get_cityscapes_loaders
 from .cifar import get_cifar10_loaders, get_cifar100_loaders
-from .bsd500 import BSD500, get_bsd500_loaders, get_bsd500_dataset
