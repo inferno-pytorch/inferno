50c0bf0419b46638e5ef0db4787580912b22679d
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2018-07-06 13:19:45 +0200

    improved models

--- inferno/extensions/models/building_blocks.py
+++ inferno/extensions/models/building_blocks.py
@@ -1,21 +1,43 @@
-class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, size=3, kernel_size=3):
+from torch.nn import Module,ModuleList,ELU
+from inferno.extensions.layers.convolutional import ConvELU2D, Conv2D,ConvELU3D, Conv3D
+import torch 
+
+class ResBlock(Module):
+    def __init__(self, in_channels, out_channels, ndim, size=3, kernel_size=3, activated=True):
         super(ResBlock, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.size = size
+        self.activated = activated
+
+        if ndim == 2:
+            self.conv_1x1 = ConvELU2D(kernel_size=1, in_channels=in_channels,   out_channels=out_channels)
+
+            conv_ops = [ConvELU2D(kernel_size=kernel_size, in_channels=in_channels,   out_channels=out_channels)]
+            for i in range(size-1):
+                if i + 1 == size-1: #<- the last
+                    conv_ops.append(ConvELU2D(kernel_size=kernel_size, in_channels=out_channels,   out_channels=out_channels))
+                else:
+                    conv_ops.append(Conv2D(kernel_size=kernel_size, in_channels=out_channels,   out_channels=out_channels))
+        elif ndim == 3:
+            self.conv_1x1 = ConvELU3D(kernel_size=1, in_channels=in_channels,   out_channels=out_channels)
+
+            conv_ops = [ConvELU3D(kernel_size=kernel_size, in_channels=in_channels,   out_channels=out_channels)]
+            for i in range(size-1):
+                if i + 1 == size-1: #<- the last
+                    conv_ops.append(ConvELU3D(kernel_size=kernel_size, in_channels=out_channels,   out_channels=out_channels))
+                else:
+                    conv_ops.append(Conv3D(kernel_size=kernel_size, in_channels=out_channels,   out_channels=out_channels))
 
 
-        conv_ops = [ConvELU2D(kernel_size=kernel_size, in_channels=in_channels,   out_channels=out_channels)]
-        for i in range(size-1):
-            conv_ops.append(ConvELU2D(kernel_size=kernel_size, in_channels=out_channels,   out_channels=out_channels))
 
-        self.conv_ops = nn.ModuleList(conv_ops)
-
+        self.conv_ops = ModuleList(conv_ops)
+        if self.activated:
+            self.non_lin = ELU()
 
     def forward(self, input):
         assert input.size(1) == self.in_channels
-
+        short = self.conv_1x1(input)
         res_list = []
         for i in  range(self.size):
             res = self.conv_ops[i](input)
@@ -24,9 +46,13 @@ class ResBlock(nn.Module):
 
         assert res_list[0].size(1)  == self.out_channels
         assert res_list[-1].size(1) == self.out_channels
-        return res_list[0] + res_list[-1]
+        non_activated =  short + res_list[-1]
+        if self.activated:
+            return self.non_lin(non_activated)
+        else:
+            return non_activated
 
-class DenseBlock(nn.Module):
+class DenseBlock(Module):
     def __init__(self, in_channels, out_channels,out_per_conv=4, size=4, kernel_size=3):
         super(DenseBlock, self).__init__()
         self.in_channels = in_channels
@@ -45,7 +71,7 @@ class DenseBlock(nn.Module):
                 conv_ops.append(ConvELU2D(kernel_size=kernel_size, in_channels=sum_of_channels,   out_channels=out_per_conv))
                 sum_of_channels += out_per_conv
 
-        self.conv_ops = nn.ModuleList(conv_ops)
+        self.conv_ops = ModuleList(conv_ops)
 
 
     def forward(self, input):
