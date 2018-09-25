faab6ccbcaf19685b73ad785243a1ff3635771f2
vzinche <valentyna.zinchenko@embl.de>
2018-09-25 13:29:42 +0200

    minor fix

--- inferno/io/volumetric/volume.py
+++ inferno/io/volumetric/volume.py
@@ -72,7 +72,7 @@ class VolumeLoader(SyncableDataset):
         else:            
             #for symmertic padding only one int can be passed for each axis
             assert_(all(isinstance(pad, (int, tuple, list)) for pad in self.padding), "Expect int or iterable", TypeError)
-            self.padding = [[pad, pad] if isinstance(int, pad) else pad for pad in self.padding]
+            self.padding = [[pad, pad] if isinstance(pad, int) else pad for pad in self.padding]
             self.volume = np.pad(self.volume,
                                  pad_width=self.padding,
                                  mode=self.padding_mode)
