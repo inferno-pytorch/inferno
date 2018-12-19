b96bf926e8d7b2c5fcb33b4559d8bb847a247d36
Nasim Rahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2018-12-19 14:10:43 +0100

    - sorry not sorry

--- inferno/extensions/__init__.py
+++ inferno/extensions/__init__.py
@@ -4,5 +4,9 @@ from . import initializers
 from . import layers
 from . import metrics
 from . import optimizers
+from . import models
+# Backward support
+from . import models as model
 
-__all__ = ['containers', 'criteria', 'initializers', 'layers', 'metrics', 'optimizers']
+__all__ = ['containers', 'criteria', 'initializers', 'layers', 'metrics', 'optimizers',
+           'models', 'model']
